96171e10a39524f5e52276a5344d4a8f9c5e3a82
Judy <jiyc1119@gmail.com>
2020-03-10 17:58:44 -0700

    created header component

---
+++ client/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Wicked Sales</h1>
+      <p>Et deserunt nisi ad nisi voluptate anim nisi magna ipsum minim qui.</p>
+    </header>
+  );
+}
