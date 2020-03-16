7fe30f329c4e39992528d062835799ea4289e72e
Judy <jiyc1119@gmail.com>
2020-03-16 00:19:53 -0700

    rough draft

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -8,27 +8,86 @@ export default class CheckoutForm extends React.Component {
       creditCard: '',
       shippingAddress: ''
     };
+
+    this.handleName = this.handleName.bind(this);
+    this.handleCreditCard = this.handleCreditCard.bind(this);
+    this.handleAddress = this.handleAddress.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleCreditCard(event) {
+    this.setState({ creditCart: event.target.creditCard });
+
+  }
+
+  handleAddress(event) {
+    this.setState({ shippingAddress: event.target.shippingAddress });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const obj = {
+      name: this.state.name,
+      creditCard: this.state.creditCard,
+      shippingAddress: this.state.shippingAddress
+    };
+    this.props.onSubmit(obj);
   }
 
   render() {
+    const array = this.props.cartItem;
+    let num = 0;
+    for (let i = 0; i < array.length; i++) {
+      num += array[i].price;
+    }
+    const total = (num / Math.pow(10, 2)).toFixed(2);
 
     return (
       <div className="container">
         <div className="col">
           <h1>My Cart</h1>
-          <p>Order Total: $</p>
-          <form >
+          <h3>Order Total: ${total}</h3>
+          <form onSubmit={this.handleSubmit}> {/* ----------form-------------- */}
+            {/* ------------name-------------- */}
+            <div>
+              <label className="my-2">
+                <p>Name</p>
+                <input type="text" value={this.state.name} onChange={this.handleName} />
+              </label>
+            </div>
+            {/* ------------cc-------------- */}
+            <div>
+              <label className="my-2">
+                <p>Credit Card</p>
+                <input type="text" value={this.state.creditCard} onChange={this.handleCreditCard} />
+              </label>
+            </div>
+            {/* ------------addy-------------- */}
+            <div>
+              <label className="my-2">
+                <p>Shipping Address</p>
+                <input type="text" value={this.state.shippingAddress} onChange={this.handleAddress} />
+              </label>
+            </div>
 
-          </form>
+            {/* ------------button-------------- */}
+            <div className="col my-4"><button type="submit" className="btn btn-primary"
+              onClick={() => this.props.setView('checkout', {})}
+            >Place Order</button></div>
+            {/* ------------button-------------- */}
+
+          </form> {/* ----------form-------------- */}
         </div>
-        <div className="col my-4"><button type="submit" className="btn btn-primary"
-          onClick={() => this.props.setView('catalog', {})}
-        >Place Order</button></div>
+
         <div onClick={() => this.props.setView('catalog', {})}>
           &lt; Back to Catalog
         </div>
       </div>
     );
   }
-// --------------------
+  // --------------------
 }
