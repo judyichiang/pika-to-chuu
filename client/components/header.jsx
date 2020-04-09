989c36061291f2038e455bcc286aaee6bdfa18e4
Judy <jiyc1119@gmail.com>
2020-04-09 11:43:25 -0700

    added pointer

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
   return (
     <header className="row">
       <div className="col-md pl-3 h1-center text-md-left"> <h3>{props.name}</h3> </div>
-      <div className="col mt-2 h3-center text-md-right" onClick={() => {
+      <div className="col mt-2 h3-center text-md-right pointer" onClick={() => {
         props.setView('cart', {});
       }} >{props.cartItemCount} Items <i className="fas fa-shopping-cart"></i>
       </div>
