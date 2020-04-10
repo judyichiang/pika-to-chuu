5226a2346d4c33278aae4f6fe785b53120961216
Judy <jiyc1119@gmail.com>
2020-04-09 22:44:10 -0700

    updated cart

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -31,7 +31,7 @@ export default function CartSummary(props) {
     const total = (num / Math.pow(10, 2)).toFixed(2);
 
     return (
-      <div className="container-fluid">
+      <div className="container">
         <div className="row">
           <div onClick={() => props.setView('catalog', {})}>
             &lt; Back to Catalog
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <header className="row">
-      <div className="col-md pl-3 h1-center text-md-left"> <h3>{props.name}</h3> </div>
-      <div className="text-right pointer col-xl-2 col-lg-3 col-md-4 col-sm-4 col-xs-4 pr-4" onClick={() => {
+    <header className="container-lg p-3 px-5 text-white bg-dark">
+      <span className="font-weight-bold">{props.name}</span>
+      <span className="float-right pointer " onClick={() => {
         props.setView('cart', {});
       }} >{props.cartItemCount} Items <i className="fas fa-shopping-cart"></i>
-      </div>
+      </span>
 
     </header>
   );
