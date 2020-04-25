b1def4a089d2acc6e8b26faa8ab3e76b4e671585
Judy <jiyc1119@gmail.com>
2020-04-24 21:07:40 -0700

    creating modal

---
+++ client/components/disclaimer.jsx
@@ -0,0 +1,37 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { HotModuleReplacementPlugin } from 'webpack';
+
+// export default class Disclaimer extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//   }
+
+//   render() {
+//     return (
+
+//       //  Modal
+//       <div className="modal fade" id="myModal" role="dialog">
+//         <div className="modal-dialog">
+
+//           {/* Modal Content */}
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h4 className="modal-title">Welcome</h4>
+//             </div>
+//             <div className="modal-body"></div>
+//             <p>Disclaimer stuff</p>
+//           </div>
+//           <div className="modal-footer">
+//             <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+//           </div>
+
+//         </div>
+//       </div>
+
+//     // -------------------
+//     );
+//   }
+
+// }
