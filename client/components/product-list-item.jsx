e75a1625226f5fc71f726423195d3b46c1fd6d1e
Judy <jiyc1119@gmail.com>
2020-04-27 14:00:49 -0700

    overflow

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -9,12 +9,12 @@ export default class ProductionListItem extends React.Component {
 
       <div className="col-md-4 mb-3"
         onClick={() => this.props.setView('details', { productId: this.props.productId })} >
-        <div className="card h-100">
-          <img className="card-img-top h-50 pointer mt-3" src={this.props.image} alt={this.props.name} />
-          <div className="card-body px-5 pb-0 pointer">
-            <h5 className="card-title pointer">{this.props.name}</h5>
-            <p className="card-subtitle text-muted pointer">${(this.props.price / Math.pow(10, 2)).toFixed(2)}</p>
-            <p className="card-text pointer">{this.props.shortDescription}</p>
+        <div className="card h-100 pointer">
+          <img className="card-img-top h-50 mt-3" src={this.props.image} alt={this.props.name} />
+          <div className="card-body px-5 pb-0 sizing">
+            <h5 className="card-title ">{this.props.name}</h5>
+            <p className="card-subtitle">${(this.props.price / Math.pow(10, 2)).toFixed(2)}</p>
+            <p className="card-text">{this.props.shortDescription}</p>
           </div>
         </div>
       </div>
