7a568088e9493d4640c8325c1da32b3cb3567b24
Judy <jiyc1119@gmail.com>
2020-04-29 23:03:06 -0700

    created bones

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 export default class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      product: null
+      product: null,
+      showModal: false
     };
+    this.modalScreen = this.modalScreen.bind(this);
 
   }
 
@@ -23,7 +24,34 @@ export default class ProductDetails extends React.Component {
       .catch(err => console.error(err));
   }
 
+  modalScreen() {
+    const product = this.state.product;
+    this.props.addToCart(product);
+    this.setState({
+      showModal: true
+    });
+  }
+
   render() {
+
+    if (this.state.showModal === true) {
+      return (
+        <div className="modal" tabIndex="-1" role="dialogue">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3 className="modal-title">Item Added</h3>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary">Back to Catalog</button>
+                <button type="button" className="btn btn-secondary">Go to Cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
     if (this.state.product) {
       return (
         <div className="container p-3">
@@ -38,7 +66,8 @@ export default class ProductDetails extends React.Component {
               <h3 className="mb-3">{this.state.product.name}</h3>
               <p className="mb-3">${(this.state.product.price / Math.pow(10, 2)).toFixed(2)}</p>
               <p>{this.state.product.shortDescription}</p>
-              <p><button className="btn btn-primary pl-3 pr-3 pt-2 pb-2" onClick={() => this.props.addToCart(this.state.product)} >Add to Cart</button></p>
+              {/* <p><button className="btn btn-primary pl-3 pr-3 pt-2 pb-2" onClick={() => this.props.addToCart(this.state.product)} >Add to Cart</button></p> */}
+              <p><button className="btn btn-primary pl-3 pr-3 pt-2 pb-2" onClick={this.modalScreen} >Add to Cart</button></p>
             </div>
           </div>
           <div className="row"></div>
